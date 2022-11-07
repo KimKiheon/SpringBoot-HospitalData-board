6fd700208fb368b6f8bf907b19e5c7c31ffb2327
Kimkiheon <cbj2741@gmail.com>
2022-11-07 14:29:54 +0900

    Article Post 추가, header footer 분리

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -1,10 +1,12 @@
 package com.mustache.bbs.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class MustacheController {
 
     @GetMapping(value = "/hi")
