8e1b4b55c61a4bcd8afa83fa40472425b7e8b0ba
Kimkiheon <cbj2741@gmail.com>
2022-11-07 11:49:47 +0900

    mustache value 전달 확인

---
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,15 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "하이하이");
+        return "greetings";
+    }
+}
