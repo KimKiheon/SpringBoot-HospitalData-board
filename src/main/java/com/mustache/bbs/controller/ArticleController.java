9d88c11de4ec175e974a24f6c45d3122991b7f14
Kimkiheon <cbj2741@gmail.com>
2022-11-09 11:22:29 +0900

    FindByID & findByAll

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,32 +5,53 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.domain.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
-@Slf4j
 @RequestMapping("/articles")
+@Slf4j
 public class ArticleController {
     private final ArticleRepository articleRepository;
+
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("")
-    public String home() {
-        return "index";
-    }
-    @GetMapping(value="/new")
-    public String newArticleForm(){
+    @GetMapping("/new")
+    public String createPage(){
         return "articles/new";
     }
-    @PostMapping("/posts")
-    public String createArticle(ArticleDTO form){
-        log.info(form.toString());
-        Article article = form.toEntity();
-        articleRepository.save(article);
-        return "";
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model){
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if(optArticle.isEmpty())return "articles/error";
+        //Optinal.get() -> Article
+        model.addAttribute("article",optArticle.get());
+        return "articles/show";
+    }
+    @PostMapping("")//articles
+    public String articles(ArticleDTO articleDTO){
+        log.info(articleDTO.getTitle());
+        log.info(articleDTO.getContent());
+        Article savedArticle = articleRepository.save(articleDTO.toEntity());
+        log.info("generatedId:{}",savedArticle.getId());
+        return String.format("redirect:/articles/%d",savedArticle.getId());
+    }
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles",articles);
+        return "articles/list";
+    }
+    @GetMapping("")
+    public String index(){
+        return "redirect:/articles/list";
     }
 }
--- src/main/java/com/mustache/bbs/domain/dto/ArticleDTO.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDTO.java
@@ -5,23 +5,15 @@ import lombok.Getter;
 
 @Getter
 public class ArticleDTO {
-    private Long id;
     private String title;
     private String content;
 
-    public ArticleDTO(Long id, String title, String content){
-        this.id=id;
-        this.title=title;
-        this.content=content;
+    public ArticleDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
-    @Override
-    public String toString(){
-        return "ArticleDTO{"+
-                "title ='" + title + '\'' +
-                ", content = '" + content + '\'' +
-                '}';
-    }
-    public Article toEntity(){
-        return new Article(title,content);
+
+    public Article toEntity() {
+        return new Article(this.title, this.content);
     }
 }
