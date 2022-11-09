272d0c2c9f0ecb26544dd61b081b11ad60f94eec
Kimkiheon <cbj2741@gmail.com>
2022-11-09 17:31:14 +0900

    Update 구현중 HttpRequestMethodNotSupportedException: Request method 'POST' not supported 에러 발생

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,10 +6,7 @@ import com.mustache.bbs.domain.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,33 +22,53 @@ public class ArticleController {
     }
 
     @GetMapping("/new")
-    public String createPage(){
+    public String createPage() {
         return "articles/new";
     }
+
     @GetMapping("/{id}")
-    public String selectSingle(@PathVariable Long id, Model model){
+    public String selectSingle(@PathVariable Long id, Model model) {
         Optional<Article> optArticle = articleRepository.findById(id);
-        if(optArticle.isEmpty())return "articles/error";
+        if (optArticle.isEmpty()) return "articles/error";
         //Optinal.get() -> Article
-        model.addAttribute("article",optArticle.get());
+        model.addAttribute("article", optArticle.get());
         return "articles/show";
     }
-    @PostMapping("")//articles
-    public String articles(ArticleDTO articleDTO){
-        log.info(articleDTO.getTitle());
-        log.info(articleDTO.getContent());
+
+    /*@PostMapping("")//articles
+    public String articles(ArticleDTO articleDTO) {
         Article savedArticle = articleRepository.save(articleDTO.toEntity());
-        log.info("generatedId:{}",savedArticle.getId());
-        return String.format("redirect:/articles/%d",savedArticle.getId());
-    }
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }*/
+
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
         List<Article> articles = articleRepository.findAll();
-        model.addAttribute("articles",articles);
+        model.addAttribute("articles", articles);
         return "articles/list";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if (optionalArticle.isEmpty()) {
+            model.addAttribute("message", String.format("%d가 없습니다 ", id));
+            return "articles/error";
+        }
+        model.addAttribute("article", optionalArticle.get());
+        return "articles/edit";
+    }
+
+    @RequestMapping(value = "/{id}/update", method = RequestMethod.PUT)
+    public String update(@PathVariable Long id, ArticleDTO articleDTO, Model model) {
+        Article article = articleRepository.save(articleDTO.toEntity());
+        model.addAttribute("article", article);
+        return String.format("redirect:/articles/%d", article.getId());
+    }
+
     @GetMapping("")
-    public String index(){
+    public String index() {
         return "redirect:/articles/list";
     }
+
 }
--- src/main/java/com/mustache/bbs/domain/dto/ArticleDTO.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDTO.java
@@ -1,17 +1,17 @@
 package com.mustache.bbs.domain.dto;
 
 import com.mustache.bbs.domain.entity.Article;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 public class ArticleDTO {
+    private Long id;
     private String title;
     private String content;
 
-    public ArticleDTO(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
 
     public Article toEntity() {
         return new Article(this.title, this.content);
